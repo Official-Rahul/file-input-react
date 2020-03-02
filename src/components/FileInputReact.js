8e50e081694530a6bba0f9eacfaf3842391d1cc2
Official-Rahul <official.rahul.email@gmail.com>
2020-03-02 19:23:34 +0530

    label & button as props

--- src/components/FileInputReact.js
+++ src/components/FileInputReact.js
@@ -1,10 +1,21 @@
 import React, { Component } from 'react'
 
 export class FileInputReact extends Component {
+    constructor(props) {
+        super(props)
+    
+        this.state = {
+             
+        }
+    }
+    
     render() {
+        const { button, label } = this.props;
         return (
             <div>
+                {label && <label for="file">{label}</label>}
                 <input id="file" type="file"></input>
+                {button && <button>{button}</button>}
             </div>
         )
     }
