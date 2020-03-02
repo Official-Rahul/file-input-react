bb18f25a232b4e0d9c959b76d65a5f575fc6951c
Official-Rahul <official.rahul.email@gmail.com>
2020-03-02 22:44:58 +0530

    file change handling

--- src/components/FileInputReact.js
+++ src/components/FileInputReact.js
@@ -6,7 +6,25 @@ export class FileInputReact extends Component {
         super(props)
     
         this.state = {
-             
+            fileChoosed:null
+        }
+    }
+
+    handleFileChange=(event)=>{
+        if(event.target.files[0]){
+            let name=event.target.files[0].name;
+            if(event.target.files[0]){
+
+            }
+            this.setState({
+                fileChoosed:event.target.files[0],
+                name:name
+            });
+        }
+        else{
+            this.setState({
+                fileChoosed:null
+            });
         }
     }
     
@@ -14,8 +32,8 @@ export class FileInputReact extends Component {
         const { button, label, hideInput } = this.props;
         return (
             <div>
-                {label && <label for="file">{label}</label>}
-                <input className={hideInput==true?'transparent':''} id="file" type="file"></input>
+                {label && <label htmlFor="file">{label}</label>}
+                <input className={hideInput==true?'transparent':''} id="file" type="file" onChange={this.handleFileChange}></input>
                 {button && <button>{button}</button>}
             </div>
         )
