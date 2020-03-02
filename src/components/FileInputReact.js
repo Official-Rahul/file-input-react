fa9af618a40bb505c9c81a329a1219e10aa9bb14
Rahul Singh <rahulsingh@197NODMB24821.one97.delhi.net>
2020-03-02 19:09:39 +0530

    unwanted commits

--- src/components/FileInputReact.js
+++ src/components/FileInputReact.js
@@ -1,22 +1,10 @@
 import React, { Component } from 'react'
 
 export class FileInputReact extends Component {
-    constructor(props) {
-        super(props)
-    
-        this.state = {
-             
-        }
-    }
-    
-    
     render() {
-        const {label, button}=this.props;
         return (
             <div>
-                {label && <label for="file">{label}</label>}
                 <input id="file" type="file"></input>
-                {button && <button>{button}</button>}
             </div>
         )
     }
